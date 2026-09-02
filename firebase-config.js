c13a78c0968e3237d4c5fff773a9c051af0838b4
Aaron Winston Gho <aaron.winston.gho@gmail.com>
2026-09-02 13:44:53 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -15,13 +15,14 @@
    (mode offline / localStorage), tidak tersinkron.
    ============================================================ */
 
-window.FIREBASE_CONFIG = {
-  apiKey: "ISI_DISINI",
-  authDomain: "ISI_DISINI.firebaseapp.com",
-  projectId: "ISI_DISINI",
-  storageBucket: "ISI_DISINI.appspot.com",
-  messagingSenderId: "ISI_DISINI",
-  appId: "ISI_DISINI"
+const firebaseConfig = {
+  apiKey: "AIzaSyBbHIE_dPMZXJL8P50-Ix_dj2bXqyukAe4",
+  authDomain: "majuterusmaju-b03f6.firebaseapp.com",
+  projectId: "majuterusmaju-b03f6",
+  storageBucket: "majuterusmaju-b03f6.firebasestorage.app",
+  messagingSenderId: "450902574757",
+  appId: "1:450902574757:web:44e4619e095dcf437d6f4c",
+  measurementId: "G-6K39NYMBLG"
 };
 
 /* (Opsional) Kode akses sederhana supaya tidak sembarang orang bisa buka.
